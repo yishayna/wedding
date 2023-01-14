f9d8780b79d545cbd835792b2a60819bb4f810a4
yishayna <44755595+yishayna@users.noreply.github.com>
2023-01-14 21:07:22 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,5 @@
-
 var common = require("./common");
 var http = require('http');
-var util = require('util');
 
 http.createServer(function (req, res) {
 
@@ -10,23 +8,19 @@ http.createServer(function (req, res) {
         return;
     } else {
         var htmlTemplate =
-            '<html> \
-                <head>\
-                    <meta charset="utf-8" />\
-                    <title> שירל וישי מתחתנים </title>\
-                    <link rel="icon" type="images/x-icon" href="https://cdn-icons-png.flaticon.com/512/3074/3074076.png" />\
-                </head>\
+        '<html> \
+            <head>\
+            <meta charset="utf-8" />\
+                <title> שירל וישי מתחתנים </title>\
+                <link rel="icon" type="images/x-icon" href="https://cdn-icons-png.flaticon.com/512/3074/3074076.png" />\
+            </head>\
+            <body>\
+            <h1> <img src="https://syweddingstorage.blob.core.windows.net/mystore/invitation.jpg"> </h1>\
+            </body>\
+        </html> \
+        ';
 
-                <body>\
-                <h1> <img src="https://syweddingstorage.blob.core.windows.net/mystore/invitation.jpg"> </h1>\
-                </body>\
-
-            </html> \
-            ';
-        
         res.writeHead(200,  { 'Content-Type': 'text/html' });
         res.end(htmlTemplate);
-        }
-    }  
-
+    }
 }).listen(process.env.PORT || 8080);
