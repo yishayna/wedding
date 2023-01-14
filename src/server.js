9933a056a5f13c54cf20ca9a28eb2ddba7502274
yishayna <44755595+yishayna@users.noreply.github.com>
2023-01-15 00:08:26 +0200

    set relative picture size

--- src/server.js
+++ src/server.js
@@ -8,17 +8,38 @@ http.createServer(function (req, res) {
         return;
     } else {
         var htmlTemplate =
-        '<html> \
-            <head>\
-            <meta charset="utf-8" />\
-                <title> שירל וישי מתחתנים </title>\
-                <link rel="icon" type="images/x-icon" href="https://cdn-icons-png.flaticon.com/512/3074/3074076.png" />\
-            </head>\
-            <body>\
-            <h1> <img src="https://syweddingstorage.blob.core.windows.net/mystore/invitation.jpg"> </h1>\
-            </body>\
-        </html> \
-        ';
+        `<html>
+            <head>
+                <style>
+                    * {
+                        padding: 0;
+                        margin: 0;
+                    }
+                    .fit { /* set relative picture size */
+                        max-width: 100%;
+                        max-height: 100%;
+                    }
+                    .center {
+                        display: block;
+                        margin: auto;
+                    }
+                </style>
+            </head>
+
+            <body>
+                <img class="center fit" src="https://syweddingstorage.blob.core.windows.net/mystore/invitation.jpg" >
+                <script src="http://code.jquery.com/jquery-latest.js">
+                <script type="text/javascript" language="JavaScript">
+                    function set_body_height() { // set body height = window height
+                        $('body').height($(window).height());
+                    }
+                    $(document).ready(function() {
+                        $(window).bind('resize', set_body_height);
+                        set_body_height();
+                    });
+                </script>
+            </body>
+        </html>`;
 
         res.writeHead(200,  { 'Content-Type': 'text/html' });
         res.end(htmlTemplate);
