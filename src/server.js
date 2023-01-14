a5a3b547e2232d41f6ef484fa887077770355444
yishayna <44755595+yishayna@users.noreply.github.com>
2023-01-14 21:00:58 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,9 +9,24 @@ http.createServer(function (req, res) {
         common.matchers[req.url](res)
         return;
     } else {
-        res.writeHead(404, { "Content-Type": "text/plain" });
-        res.write("link not found");
-        res.end();
+        var htmlTemplate =
+            '<html> \
+                <head>\
+                    <meta charset="utf-8" />\
+                    <title> שירל וישי מתחתנים </title>\
+                    <link rel="icon" type="images/x-icon" href="https://cdn-icons-png.flaticon.com/512/3074/3074076.png" />\
+                </head>\
+
+                <body>\
+                <h1> <img src="https://syweddingstorage.blob.core.windows.net/mystore/invitation.jpg"> </h1>\
+                </body>\
+
+            </html> \
+            ';
+        
+        res.writeHead(200,  { 'Content-Type': 'text/html' });
+        res.end(htmlTemplate);
+        }
     }  
 
 }).listen(process.env.PORT || 8080);
