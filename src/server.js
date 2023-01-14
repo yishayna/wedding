909519ea04e575a4ac49e1f9bf66a74a31d87d84
yishayna <44755595+yishayna@users.noreply.github.com>
2023-01-14 17:56:41 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
 
 var common = require("./common");
 var http = require('http');
+var util = require('util');
 
 http.createServer(function (req, res) {
-    for (var matcher of common.matchers) {
-        if (req.url.startsWith(matcher.uriPrefix)) {
-            matcher.callback(res)
-            return;
-        }
-    }
-    
-    // reach here only if there is no match
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.write("link not found");
-    res.end();
-}).listen(process.env.PORT || 80);
+
+    if (req.url in common.matchers){
+        common.matchers[req.url](res)
+        return;
+    } else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.write("link not found");
+        res.end();
+    }  
+
+}).listen(process.env.PORT || 8080);
