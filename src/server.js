59d87f69790125e75a7af03f4cf12804690e0c29
yishayna <44755595+yishayna@users.noreply.github.com>
2023-01-14 16:52:41 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,6 +7,12 @@ http.createServer(function (req, res) {
     for (var matcher of common.matchers) {
         if (req.url.startsWith(matcher.uriPrefix)) {
             matcher.callback(res)
+            return;
         }
     }
+    
+    // reach here only if there is no match
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.write("link not found");
+    res.end();
 }).listen(process.env.PORT || 80);
