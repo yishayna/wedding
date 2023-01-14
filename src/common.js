71e239d3833365c721972d7816ea538fbc05667b
yishayna <44755595+yishayna@users.noreply.github.com>
2023-01-14 17:13:09 +0200

    Update common.js

--- src/common.js
+++ src/common.js
@@ -1,9 +1,11 @@
+var util = require('util');
+
 function createPath(uriPrefix, proto, linkPath) {
 
     var htmlTemplate =
     '<html> \
         <head> \
-            <meta name="redirect" content="weddinglinkresolver.azurewebsites.net%s %s %s"> \
+            <meta name="redirect" content="wedding-link-resolver.azurewebsites.net%s %s %s"> \
             <meta http-equiv="refresh" content="0;URL=\'%s\'" /> \
         </head> \
         <body> \
@@ -25,7 +27,7 @@ function createPath(uriPrefix, proto, linkPath) {
 var matchers = [
 	createPath("/gift", 
         "PAYBOX", 
-        "https://payboxapp.page.link/?link=http://pbme.co/?v%3Dj%26g%3D63c183c483853700087aba53&apn=com.payboxapp&afl=http://pbme.co/?v%3Dj%26g%3D63c183c483853700087aba53&ibi=com.payboxapp.paybox&ifl=http://pbme.co/?v%3Dj%26g%3D63c183c483853700087aba53&cid=2042653307573259374&_osl=https://payboxapp.page.link/r4mqH9bk82XSVyfz8&_icp=1")
+        "https://payboxapp.page.link/?link=http://pbme.co/?v%3Dj%26g%3D63c183c483853700087aba53&apn=com.payboxapp&afl=http://pbme.co/?v%3Dj%26g%3D63c183c483853700087aba53&ibi=com.payboxapp.paybox&ifl=http://pbme.co/?v%3Dj%26g%3D63c183c483853700087aba53&cid=2042653307573259374&_osl=https://payboxapp.page.link/r4mqH9bk82XSVyfz8&_icp=1"),
 ];
 
 module.exports = {
