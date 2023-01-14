9cbd9dee62dfa5f0e3389e505428cbadeee6bc18
yishayna <44755595+yishayna@users.noreply.github.com>
2023-01-14 16:37:49 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,20 +4,6 @@ var http = require('http');
 var util = require('util');
 
 http.createServer(function (req, res) {
-    var htmlTemplate =
-        '<html> \
-            <head> \
-                <meta name="redirect" content="weddinglinkresolver.azurewebsites.net%s %s %s"> \
-                <meta http-equiv="refresh" content="0;URL=\'%s\'" /> \
-            </head> \
-            <body> \
-                <p>This link you are looking for is <a href="%s"> here</a>. \
-                </p> \
-            </body> \
-        </html> \
-        ';
-   
-
     for (var matcher of common.matchers) {
         if (req.url.startsWith(matcher.uriPrefix)) {
             matcher.callback()
